014fbc417c6a9a6270912a1f7fd55b6bcb5eaa48
Trevor Vardeman <trevortx@gmail.com>
2022-03-16 23:56:37 -0700

    commit tree 123bedcb18e4169ce3366b5922eee4a47db480ee

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (i = 0; i < array.length; i++) {
+    const comp = target - array[i]
+    for (let j = i + 1; j < array.length; j++)
+      if (array[j] === comp) return true
+  }
+  return false
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  first for loop = n steps
+  second loop = n * n steps
+  runtime: 0(n^2)
+  space: 0(n)
 */
 
 /* 
-  Add your pseudocode here
+  take in a target array and a value as arguments
+  iterate over the array, start with the first index and add that to the next, and so on,
+  to see any additions match the target
+  return true if two values = the target, else return false
 */
 
 /*
-  Add written explanation of your solution here
+  create a for loop to use to search the array's indexes
+  create a complementary variable that's our target number
+    if we find this number in the second for loop, we'll have a match and can return true!
+  create another for loop to compare the index +1 of the first for loop
+  if the complementary variable is equal to any of the indexes from the second for loop, return true
+  else return false
 */
 
 // You can run `node index.js` to view these console logs
